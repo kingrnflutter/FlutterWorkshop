fbd7dfd2806657c035af68375ae591dbaa3dcaa6
kingrnflutter <greatcgdev007@gmail.com>
2021-11-08 18:09:54 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:apprev/list.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: "First App",
+      home: firstApp(),
+      debugShowCheckedModeBanner: false,
+    )
+
+  );
+}
+
+class firstApp extends StatelessWidget {
+  const firstApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F0D7),
+      appBar: AppBar(
+        title: const Text("WORD APP",style: TextStyle(fontFamily: "Roboto",
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 22),),
+        centerTitle: true,
+        backgroundColor: const Color(0xFFFED2AA),
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: animals.length,
+
+          itemBuilder: (BuildContext context, int index){
+            return Padding(
+              padding: const EdgeInsets.only(top: 8.0,bottom: 8),
+              child: ListTile(
+                tileColor: const Color(0xFFCEE5D0),
+                title: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(animals[index]["animal"],style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                ),
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => animalDetails(
+                        animalName: animals[index]["animal"],
+                        animalDescription: animals[index]["description"],
+                        animalStatus: animals[index]["status"],
+                        animalURL: animals[index]["img"])),
+                  );
+                },
+              ),
+            );
+          }),
+      ),
+
+    );
+  }
+}
+
+
+class animalDetails extends StatelessWidget {
+  const animalDetails({Key? key, required this.animalName, required this.animalDescription, required this.animalStatus, required this.animalURL}) : super(key: key);
+  final String animalName;
+  final String animalDescription;
+  final String animalStatus;
+  final String animalURL;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F0D7),
+      appBar: AppBar(
+        title: const Text("Animal Description",style: TextStyle(fontFamily: "Roboto",
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 22),),
+        centerTitle: true,
+        backgroundColor: const Color(0xFFFED2AA),
+      ),
+      body: Column(
+        children: [
+          Image.network(animalURL),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(animalName,style: const TextStyle(fontSize: 19,fontWeight: FontWeight.bold),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(animalStatus,style: const TextStyle(fontSize: 10,fontWeight: FontWeight.bold,letterSpacing: 1.5)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(animalDescription,style: const TextStyle(fontSize: 14,fontWeight: FontWeight.bold,letterSpacing: 1.5)),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
