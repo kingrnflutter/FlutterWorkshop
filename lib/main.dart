73151c79cae8b2dd19792213059a570a18f985a7
kingrnflutter <greatcgdev007@gmail.com>
2021-11-09 10:09:36 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:apprev/list.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+
 
 void main() {
   runApp(
     const MaterialApp(
       title: "First App",
-      home: firstApp(),
+      home: splashScreen(),
       debugShowCheckedModeBanner: false,
     )
 
@@ -100,5 +102,32 @@ class animalDetails extends StatelessWidget {
     );
   }
 }
+
+
+
+class splashScreen extends StatelessWidget {
+  const splashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: Image.asset("assets/images/turtle.png"),
+      title: const Text(
+        "App Title",
+        style: TextStyle(
+          fontSize: 21,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor:  const Color(0xFFF3F0D7),
+      showLoader: true,
+      loaderColor: const Color(0xFFCEE5D0) ,
+      loadingText: const Text("Loading..."),
+      navigator: const firstApp(),
+      durationInSeconds: 5,
+    );
+  }
+}
+
 
 
